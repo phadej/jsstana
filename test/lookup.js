b99e439d4bd12bf70ff26cb35da80c7379fc5b68
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-10-31 13:18:05 +0200

    Shorthand lookup syntax

--- test/lookup.js
+++ test/lookup.js
@@ -37,3 +37,35 @@ describe("lookup", function () {
     assert.equal(m.property.name, "bar");
   });
 });
+
+describe("shorthand lookup", function () {
+  it("makes it easier to work with properties", function () {
+    var syntax = esprima.parse("foo.bar.baz");
+    var node = syntax.body[0].expression;
+    var matcher = jsstana.match("foo.bar.baz");
+
+    assert.deepEqual(matcher(node), {});
+  });
+
+  it("makes it easier to work with properties 2", function () {
+    var syntax = esprima.parse("foo.quux.baz");
+    var node = syntax.body[0].expression;
+    var matcher = jsstana.match("foo.bar.baz");
+
+    assert.deepEqual(matcher(node), undefined);
+  });
+
+  it("makes it easier to work with properties 2", function () {
+    var syntax = esprima.parse("foo.bar");
+    var node = syntax.body[0].expression;
+    var matcher = jsstana.match("?object.?property");
+
+    var m = matcher(node);
+
+    assert.equal(m.object.type, "Identifier");
+    assert.equal(m.property.type, "Identifier");
+
+    assert.equal(m.object.name, "foo");
+    assert.equal(m.property.name, "bar");
+  });
+});
