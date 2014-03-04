4094bec47485e515f75941bc6a2bd414b5eff079
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-03-04 09:13:12 +0200

    this

---
+++ test/this.js
@@ -0,0 +1,27 @@
+/* global describe:true, it:true */
+"use strict";
+
+var jsstana = require("../lib/jsstana.js");
+
+var assert = require("assert");
+var esprima = require("esprima");
+ 
+describe("this", function () {
+  it("matches operator", function () {
+    var syntax = esprima.parse("this;");
+    var node = syntax.body[0].expression;
+    var matcher = jsstana.match("(this)");
+
+    assert.deepEqual(matcher(syntax), undefined);
+    assert.deepEqual(matcher(node), {});
+  });
+
+  it("matches operator", function () {
+    var syntax = esprima.parse("this;");
+    var node = syntax.body[0].expression;
+    var matcher = jsstana.match("this");
+
+    assert.deepEqual(matcher(syntax), undefined);
+    assert.deepEqual(matcher(node), {});
+  });
+});
